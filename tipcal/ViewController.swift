a1af4e7216fe4c9080ed92e18ae8cbc0e9b41e8c
Chee Jun Wong <cheejunwong@hotmail.com>
2020-01-24 18:29:06 -0800

    Initial Commit

---
+++ tipcal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcal
+//
+//  Created by Chee Jun Wong on 1/24/20.
+//  Copyright © 2020 Chee Jun Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
